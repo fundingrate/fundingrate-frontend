3401940eee4d9b1200cb9293246b28fa73ff32a3
Jacob Dent <jdent@tacyarg.com>
2019-10-03 08:07:37 -0400

    markdownpages

--- src/Layout.js
+++ src/Layout.js
@@ -3,16 +3,31 @@ import React, { useEffect, useState } from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Assets from './components/Assets'
-import { Button, Flex, Box, Text, Image, Sidebar, Page } from './primitives'
+import {
+  Button,
+  Flex,
+  Box,
+  Text,
+  Image,
+  Sidebar,
+  Page,
+  Divider,
+} from './primitives'
 
 import Pages from './pages'
 
 const SideNav = ({ links, onClick }) => {
   return (
     <Sidebar>
-      <Flex alignItems="center" justifyContent="center" my={3} onClick={e => onClick('/home')}>
+      <Flex
+        alignItems="center"
+        justifyContent="center"
+        my={3}
+        onClick={e => onClick('/home')}
+      >
         <Image mr={2} src={Assets.Icons.Popular} size={28} /> Dashboard
       </Flex>
+      <Divider />
       {links.map(({ label, href }) => {
         return (
           <Button key={label} type="simple" onClick={e => onClick(href)}>
