17b0d5d3c247e2d6eb39a704af926b78a85a561d
Jacob Dent <jdent@tacyarg.com>
2019-10-04 07:32:42 -0400

    provider subscriptions

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,14 @@ import Documentation from './Documentation'
 import FAQ from './FAQ'
 // import Filters from './Filters'
 import Providers from './Providers'
+import Subscriptions from './Subscriptions'
 
 
 export default {
     Home,
     Profile,
     Providers,
+    Subscriptions,
     Events,
     Trades,
     Stats,
