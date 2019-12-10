7e438c90e2f12cea1ee454c422edd317ee427ba0
Jacob Dent <jdent@tacyarg.com>
2019-12-10 16:49:59 -0400

    better 404 pages

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { Button, Text, Box, Flex, Divider } from '../primitives'
 
 export default ({ history }) => {
-  history.push('/')
-  window.location.reload()
+  setTimeout(history.goBack, 1000)
+  // window.location.reload()
+  return <Box m={4}>
+    <Text.Heading>404</Text.Heading>
+    <Text>Redirecting...</Text>
+  </Box>
 }
