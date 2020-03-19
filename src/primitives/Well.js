dab7c066a3591c8e9793672bcdb49e89844deef9
Jacob Dent <jdent@tacyarg.com>
2020-03-19 04:40:20 +0000

    new prim

---
+++ src/primitives/Well.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import { Flex, Box, Button, Image } from './index'
+import theme from "../styles/theme";
+
+const Well = styled(Box)`
+  box-shadow: inset 0px 0px 10px rgba(0,0,0,0.5);
+  border: ${theme.colors.backingDark} ${theme.borders.normal};
+  border-radius: ${theme.radii.rounded};
+`
+
+Well.defaultProps = {
+  as: 'code',
+  // bg: "foregroundBacking",
+  bg: 'darkBacking',
+  p: 2,
+}
+
+export default Well
